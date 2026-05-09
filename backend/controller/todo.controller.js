e93d216a3de52ebb3a44e2a34cc0717139ced35e
Muneeb <muneebilyas83217@example.com>
2026-05-09 09:31:39 +0500

    frontend created

--- backend/controller/todo.controller.js
+++ backend/controller/todo.controller.js
@@ -3,12 +3,13 @@ import Todo from "../model/todo.js";
 export const createTodo = async (req, res) => {
 
     try {
-        const { title } = req.body;
+        const { title, completed } = req.body;
         if (!title) {
             return res.json({ message: "Title field is required" })
         }
         const newTodo = await Todo.create({
-            title
+            title,
+            completed: completed || false
         })
         res.json({
             success: true, todo: newTodo
